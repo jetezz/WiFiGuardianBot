bf38fd472c23d2cb3486fdf24ee10d0069cec3ad
jetezz <cuadrabc2015@gmail.com>
2024-07-12 18:21:27 +0200

    header

--- service_fetch.py
+++ service_fetch.py
@@ -1,21 +1,23 @@
 import requests
 from enum import Enum
 
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+}
+
 class States(Enum):
     OK = 1
     REQUEST_ERROR = 2
     TIMEOUT_ERROR = 3
     SERVIDOR_ERROR = 4
 
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
-    }
+    
 
 def fetch_server_data(url):   
     global headers
     try:
         # Realiza la solicitud HTTP GET con un tiempo límite de 5 segundos
-        response = requests.get(url,headers = headers, timeout=5)
+        response = requests.get(url, headers=headers, timeout=5)
 
         print(response)
         
